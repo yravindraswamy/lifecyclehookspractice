bf0278391d311699c670d11cbbf68a274e23203a
Ravindraswamy <82256542+yravindraswamy@users.noreply.github.com>
2023-05-14 10:03:26 +0530

    life cycle hooks added

---
+++ src/components/ChildComponent.vue
@@ -0,0 +1,42 @@
+<template>
+    <div>
+        <h2>Child Component</h2>
+    </div>
+</template>
+
+<script>
+    export default {
+        name:"ChildComponent",
+        beforeCreate() {
+            console.log(`child component before create hook()`);
+        },
+        created() {
+            console.log(`child component create hook()`);
+        },
+        beforeMount() {
+            alert('you are mounting child component');
+            console.log(`child component before mount hook()`);
+        },
+        mounted() {
+            console.log(`child component mounted hook()`);
+        },
+        beforeUpdate() {
+            console.log(`child component before update hook()`);
+        },
+        updated() {
+            console.log(`child component updated hook()`);
+        },
+        beforeUnmount(){
+            alert("you are unmounting childComponent");
+            console.log(`child component beforeUnmount hook()`);
+        },
+        unmounted(){
+            alert("you are unmounted childComponent");
+            console.log(`child component unmounted hook()`);
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/components/ParentComponent.vue
@@ -0,0 +1,51 @@
+<template>
+    <div>
+        <h2>Parent Component</h2>
+        <button @click="child = !child">Toogle button</button>
+        <ChildComponent v-show="child"/>
+    </div>
+</template>
+
+<script>
+import ChildComponent from './ChildComponent.vue';
+    export default {
+        name:"ParentComponent",
+        components:{
+            ChildComponent
+        },
+        data(){
+            return {
+                child:true
+            }
+        },
+        beforeCreate() {
+            console.log(`parent component before create hook()`);
+        },
+        created() {
+            console.log(`parent component create hook()`);
+        },
+        beforeMount() {
+            console.log(`parent component before mount hook()`);
+        },
+        mounted() {
+            console.log(`parent component mounted hook()`);
+        },
+        beforeUpdate() {
+            console.log(`parent component before update hook()`);
+        },
+        updated() {
+            console.log(`parent component updated hook()`)
+        },
+        beforeUnmount(){
+            console.log(`parent Component beforeUnmount hook()`);
+        },
+        unmounted(){
+            console.log(`parent Component unmounted hook()`);
+        }
+
+    }
+</script>
+
+<style scoped>
+
+</style>
